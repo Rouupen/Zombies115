346a28ddfdfef5651361591e8677cf460c9c998d
Rouupen <nebur.nds@gmail.com>
2025-04-04 16:16:10 +0200

    Scene changes and more actions for the character

--- Assets/Scripts/StateMachine/CharacterStates.cs
+++ Assets/Scripts/StateMachine/CharacterStates.cs
@@ -43,3 +43,93 @@ public class Running : State
     }
 }
 
+public class Crouching : State
+{
+    public override void OnEnter()
+    {
+        Debug.Log("OnEnter Crouching");
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("OnUpdate Crouching");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("OnExit Crouching");
+    }
+}
+
+public class Crawling : State
+{
+    public override void OnEnter()
+    {
+        Debug.Log("OnEnter Crawling");
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("OnUpdate Crawling");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("OnExit Crawling");
+    }
+}
+
+public class Jumping : State
+{
+    public override void OnEnter()
+    {
+        Debug.Log("OnEnter Jumping");
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("OnUpdate Jumping");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("OnExit Jumping");
+    }
+}
+
+public class OnAir : State
+{
+    public override void OnEnter()
+    {
+        Debug.Log("OnEnter OnAir");
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("OnUpdate OnAir");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("OnExit OnAir");
+    }
+}
+
+public class Interacting : State
+{
+    public override void OnEnter()
+    {
+        Debug.Log("OnEnter Interacting");
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("OnUpdate Interacting");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("OnExit Interacting");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Zombies115
 #This prototype is a recreation of Call of Duty's zombie mode in Unity 6
 #Unity 6000.0.20f1
-#Time spent: 5h
+#Time spent: 7.5h
