0963387540d04debe46b73da52d76c51ad8f4f87
Rouupen <nebur.nds@gmail.com>
2025-04-15 21:50:12 +0200

    Weapon walking and recoiling fixed

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -10,7 +11,7 @@ public class Weapon : MonoBehaviour
 {
     [Header("---FAST TESTING---")]
     [Header("Look Inertia")]
-    public Vector2 m_maxAngleInertia = new Vector2(30,15);
+    public Vector2 m_maxAngleInertia = new Vector2(30, 15);
     public float m_maxTranslationInertia = 10.0f;
     public float m_lookInertiaRotationSpeed = 5.0f;
     public float m_lookInertiaTraslationSpeed = 5.0f;
@@ -22,21 +23,6 @@ public class Weapon : MonoBehaviour
     private Quaternion _rotationLookInertia;
     private Coroutine _returnToPositionInertia;
 
-
-    [Header("Recoil Inertia")]
-    public float m_recoilUpPositionSpeed = 0.05f;
-    public float m_recoilRightRotationSpeed = -20f;
-    public float m_recoilTime = 0.05f;
-    public float m_recoilRecoverySpeedTime = 0.15f;
-    public Vector2 m_randomRotationInertia = new Vector2(1, 5);
-    public AnimationCurve m_recoilCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
-
-    [Header("Aim")]
-    public Vector3 m_aimPosition;
-    public Quaternion m_aimRotation;
-    public float m_startAimTime = 0.2f;
-    public float m_endAimTime = 0.2f;
-    public float m_inertiaReductionWhileAiming = 0.1f;
     [Header("Idle")]
     public Vector3 m_localPositionWeaponIdle;
     public Quaternion m_localRotationWeaponIdle;
@@ -68,9 +54,30 @@ public class Weapon : MonoBehaviour
     private float _currentVerticalBounceTime = 0f;
     private float _currentHorizontalBounceTime = 0f;
     private float _currentForwardRotationTime = 0f;
-    private Vector3 _currentDirecitionTranslation;
+    private Vector3 _currentDirectionTranslation;
     private Coroutine _startEndWalking;
     private bool _isWalking;
+
+
+    [Header("Recoil Inertia")]
+    public Vector3 m_maxRecoilRotation = new Vector3(60.0f, 0, 0);
+    public Vector3 m_maxRecoilPosition = new Vector3(0, 0.5f, 0);
+    public Vector3 m_recoilAdditivePosition = new Vector3(0, 0.05f, 0);
+    public Vector3 m_recoilAdditiveRotation = new Vector3(20.0f, 0, 0);
+    public float m_recoilStartTime = 0.05f;
+    public float m_recoilRecoverySpeedTime = 0.15f;
+    public Vector2 m_randomRotationRecoilInertiaY = new Vector2(1, 5);
+    public AnimationCurve m_recoilRotationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    public AnimationCurve m_recoilPositionCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    public AnimationCurve m_recoveryRecoilCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    [Header("Aim")]
+    public Vector3 m_aimPosition;
+    public Quaternion m_aimRotation;
+    public float m_startAimTime = 0.2f;
+    public float m_endAimTime = 0.2f;
+    public float m_inertiaReductionWhileAiming = 0.1f;
+
 
     [Header("Damping")]
     public AnimationCurve m_dampingCurve;
@@ -93,10 +100,14 @@ public class Weapon : MonoBehaviour
 
     //Temp
     bool _wasGrounded = true;
+    private Vector3 _startEndWalkingPositionOffset;
+    private Quaternion _startEndWalkingRotationOffset = Quaternion.identity;
     private Vector3 _walkingPositionOffset;
-    private Quaternion _walkingRotationOffset;
+    private Quaternion _walkingRotationOffset = Quaternion.identity;
     private Vector3 _inertiaPositionOffset;
-    private Quaternion _inertiaRotationOffset;
+    private Quaternion _inertiaRotationOffset = Quaternion.identity;
+    private Vector3 _recoilPositionOffset;
+    private Quaternion _recoilRotationOffset = Quaternion.identity;
 
     private void Start()
     {
@@ -120,15 +131,15 @@ public class Weapon : MonoBehaviour
         }
         _wasGrounded = GameManager.GetInstance().m_playerController.m_characterController.isGrounded;
 
-        if (_isRecoiling /*|| _isAiming*/)
-        {
-            return;
-        }
+        //if (_isRecoiling /*|| _isAiming*/)
+        //{
+        //    return;
+        //}
         UpdateInertia();
         WalkingLoop();
 
-        _socket.localPosition = _currentSocketPosition + _inertiaPositionOffset + _walkingPositionOffset;
-        _socket.localRotation = _currentSocketRotation * _inertiaRotationOffset * _walkingRotationOffset;
+        _socket.localPosition = _currentSocketPosition + _inertiaPositionOffset + _walkingPositionOffset + _recoilPositionOffset;
+        _socket.localRotation = _currentSocketRotation * _inertiaRotationOffset * _walkingRotationOffset * _recoilRotationOffset;
     }
     private Vector2 _lookVelocitySmooth = Vector2.zero;
     private Vector2 _lookVelocityRef = Vector2.zero;
@@ -215,11 +226,11 @@ public class Weapon : MonoBehaviour
     private IEnumerator StartEndWalkAnimation(bool starting)
     {
         float currentTime = 0;
-        Vector3 endPosition = starting ? m_localPositionWeaponWalking : m_localPositionWeaponIdle;
-        Quaternion endRotation = starting ? m_localRotationWeaponWalking : m_localRotationWeaponIdle;
+        Vector3 startPosition = starting ? _walkingPositionOffset : _walkingPositionOffset;
+        Vector3 endPosition = starting ? m_localPositionWeaponWalking - m_localPositionWeaponIdle : Vector3.zero;
 
-        Vector3 startPosition = _socket.localPosition;
-        Quaternion startRotation = _socket.localRotation;
+        Quaternion startRotation = starting ? Quaternion.identity : m_localRotationWeaponWalking;
+        Quaternion endRotation = starting ? m_localRotationWeaponWalking : Quaternion.identity;
         while (currentTime <= m_localPositionWeaponWalkingTime)
         {
             float valueCurve = m_localPositionWalkingTranslationCurve.Evaluate((currentTime / m_localPositionWeaponWalkingTime) % 1);
@@ -227,14 +238,14 @@ public class Weapon : MonoBehaviour
             Vector3 currentPosition = Vector3.Slerp(startPosition, endPosition, valueCurve);
             Quaternion currentRotation = Quaternion.Slerp(startRotation, endRotation, valueCurve);
 
-            _currentSocketPosition = currentPosition;
-            _currentSocketRotation = currentRotation;
+            _startEndWalkingPositionOffset = currentPosition;
+            _startEndWalkingRotationOffset = currentRotation;
 
             currentTime += Time.deltaTime;
             yield return null;
         }
-        _currentSocketPosition = endPosition;
-        _currentSocketRotation = endRotation;
+        _startEndWalkingPositionOffset = endPosition;
+        _startEndWalkingRotationOffset = endRotation;
         _startEndWalking = null;
     }
     private void WalkingLoop()
@@ -278,17 +289,13 @@ public class Weapon : MonoBehaviour
                 charDirection.z = -charDirection.y * m_translationForwardDistance;
                 charDirection.y = 0;
 
-                _currentDirecitionTranslation += charDirection * m_translationSpeed * Time.deltaTime;
+                _currentDirectionTranslation += charDirection * m_translationSpeed * Time.deltaTime;
 
-                _currentDirecitionTranslation.x = Mathf.Clamp(_currentDirecitionTranslation.x, -m_translationHorizontalDistance, m_translationHorizontalDistance);
-                _currentDirecitionTranslation.z = Mathf.Clamp(_currentDirecitionTranslation.z, -m_translationForwardDistance, m_translationForwardDistance);
+                _currentDirectionTranslation.x = Mathf.Clamp(_currentDirectionTranslation.x, -m_translationHorizontalDistance, m_translationHorizontalDistance);
+                _currentDirectionTranslation.z = Mathf.Clamp(_currentDirectionTranslation.z, -m_translationForwardDistance, m_translationForwardDistance);
 
-                _walkingPositionOffset = newPosition + _currentDirecitionTranslation;
-                _walkingRotationOffset = newRotation;
-
-                //Move and rotate
-                //_socket.localPosition = _currentSocketPosition + newPosition + _currentDirecitionTranslation;
-                //_socket.localRotation = _currentSocketRotation * newRotation;
+                _walkingPositionOffset = newPosition + _currentDirectionTranslation + _startEndWalkingPositionOffset;
+                _walkingRotationOffset = newRotation * _startEndWalkingRotationOffset;
             }
             else
             {
@@ -296,10 +303,12 @@ public class Weapon : MonoBehaviour
                 {
                     StartEndWalking(false);
                 }
+                _walkingPositionOffset = _startEndWalkingPositionOffset;
+                _walkingRotationOffset = _startEndWalkingRotationOffset;
                 _currentVerticalBounceTime = 0f;
                 _currentHorizontalBounceTime = 0f;
                 _currentForwardRotationTime = 0f;
-                _currentDirecitionTranslation = Vector3.zero;
+                _currentDirectionTranslation = Vector3.zero;
             }
 
         }
@@ -307,11 +316,11 @@ public class Weapon : MonoBehaviour
 
     private void Fire(InputAction.CallbackContext context)
     {
-        //if (_coroutineFire != null)
-        //{
-        //    StopCoroutine(_coroutineFire);
-        //}
-        //_coroutineFire = StartCoroutine(FireAnimation());
+        if (_coroutineFire != null)
+        {
+            StopCoroutine(_coroutineFire);
+        }
+        _coroutineFire = StartCoroutine(FireAnimation());
     }
 
     IEnumerator FireAnimation()
@@ -319,30 +328,47 @@ public class Weapon : MonoBehaviour
         _isRecoiling = true;
         float _currentRecoilSpeedTime = 0;
         float _currentRecoilRecoverySpeedTime = 0;
-        Vector3 currentPosition = _currentSocketPosition;
-        Quaternion currentRotation = _socket.localRotation;
-        currentRotation.y = _currentSocketRotation.y;
-        float recoilInertiaReduction = _isAiming ? m_inertiaReductionWhileAiming : 1;
-        Vector2 randomRotation = new Vector2(UnityEngine.Random.Range(-1, 1.0f), UnityEngine.Random.Range(-1, 1.0f)) * UnityEngine.Random.Range(m_randomRotationInertia.x, m_randomRotationInertia.y);
-        while (_currentRecoilSpeedTime <= m_recoilTime)
+
+        Vector3 startPosition = _recoilPositionOffset;
+        Vector3 endPosition = _recoilPositionOffset + m_recoilAdditivePosition;
+        endPosition.x = Mathf.Clamp(endPosition.x, -m_maxRecoilPosition.x, m_maxRecoilPosition.x);
+        endPosition.y = Mathf.Clamp(endPosition.y, -m_maxRecoilPosition.y, m_maxRecoilPosition.y);
+        endPosition.z = Mathf.Clamp(endPosition.z, -m_maxRecoilPosition.z, m_maxRecoilPosition.z);
+
+        Vector3 randomRotation = new Vector3(UnityEngine.Random.Range(-1, 1.0f), UnityEngine.Random.Range(-1, 1.0f), 0) * UnityEngine.Random.Range(m_randomRotationRecoilInertiaY.x, m_randomRotationRecoilInertiaY.y);
+        Quaternion startRotation = _recoilRotationOffset;
+        Vector3 endRotationClamped = _recoilRotationOffset.eulerAngles + m_recoilAdditiveRotation + randomRotation;
+
+        endRotationClamped.x = Mathf.Clamp(endRotationClamped.x > 180 ? endRotationClamped.x - 360 : endRotationClamped.x, -m_maxRecoilRotation.x, m_maxRecoilRotation.x);
+        endRotationClamped.y = Mathf.Clamp(endRotationClamped.y > 180 ? endRotationClamped.y - 360 : endRotationClamped.y, -m_maxRecoilRotation.y, m_maxRecoilRotation.y);
+        endRotationClamped.z = Mathf.Clamp(endRotationClamped.z > 180 ? endRotationClamped.z - 360 : endRotationClamped.z, -m_maxRecoilRotation.z, m_maxRecoilRotation.z);
+        Quaternion endRotation = Quaternion.Euler(endRotationClamped);
+
+        while (_currentRecoilSpeedTime <= m_recoilStartTime)
         {
-            float t = _currentRecoilSpeedTime / m_recoilTime;
-            float curveT = m_recoilCurve.Evaluate(t);
-            _socket.localPosition = Vector3.Slerp(currentPosition, currentPosition + Vector3.up * m_recoilUpPositionSpeed * recoilInertiaReduction, curveT);
-            Quaternion recoilRot = Quaternion.Euler(m_recoilRightRotationSpeed * recoilInertiaReduction, randomRotation.y, randomRotation.x);
-            _socket.localRotation = Quaternion.Slerp(currentRotation, currentRotation * recoilRot, curveT);
+            float curveRotationT = m_recoilRotationCurve.Evaluate(_currentRecoilSpeedTime / m_recoilStartTime);
+            float curvePositionT = m_recoilPositionCurve.Evaluate(_currentRecoilSpeedTime / m_recoilStartTime);
+           
+            _recoilPositionOffset = Vector3.Slerp(startPosition, endPosition, curvePositionT);
+            _recoilRotationOffset = Quaternion.Slerp(startRotation, endRotation, curveRotationT);
+
             _currentRecoilSpeedTime += Time.deltaTime;
             yield return null;
         }
 
         while (_currentRecoilRecoverySpeedTime <= m_recoilRecoverySpeedTime)
         {
-            _socket.localPosition = Vector3.Slerp(currentPosition + Vector3.up * m_recoilUpPositionSpeed * recoilInertiaReduction, currentPosition, _currentRecoilRecoverySpeedTime / m_recoilRecoverySpeedTime);
-            _socket.localRotation = Quaternion.Slerp(_socket.localRotation, currentRotation, _currentRecoilRecoverySpeedTime / m_recoilRecoverySpeedTime);
+            float curveT = m_recoveryRecoilCurve.Evaluate(_currentRecoilRecoverySpeedTime / m_recoilRecoverySpeedTime);
+            _recoilPositionOffset = Vector3.Slerp(endPosition, Vector3.zero, curveT);
+            _recoilRotationOffset = Quaternion.Slerp(endRotation, Quaternion.identity, curveT);
+
             _currentRecoilRecoverySpeedTime += Time.deltaTime;
             yield return null;
 
         }
+
+        _recoilPositionOffset = Vector3.zero;
+        _recoilRotationOffset = Quaternion.identity;
         _isRecoiling = false;
         _coroutineFire = null;
     }
